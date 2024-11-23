8f1207965d9b879041e4dbf482ff8cb1be733b93
GameDokter51 <fakemcfakeson0@gmail.com>
2024-11-23 01:03:50 -0500

    I HAVE NO SANITY LEFT SOMEONE PLEASE HELP ME
    I'll MAKE A DEAL WITH SINYTRA, PRETEND TO BE A FURRY, I'LL DO ANYTHING!

--- src/main/java/net/stasis/shatteredsoul/init/ShatteredSoulSounds.java
+++ src/main/java/net/stasis/shatteredsoul/init/ShatteredSoulSounds.java
@@ -13,5 +13,6 @@ public class ShatteredSoulSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ShatteredSoul.MODID);
 	public static final RegistryObject<SoundEvent> SUSPENSE_STRIKE = REGISTRY.register("suspense_strike", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("shatteredsoul", "suspense_strike")));
 	public static final RegistryObject<SoundEvent> BELL_TOLL = REGISTRY.register("bell_toll", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("shatteredsoul", "bell_toll")));
+	public static final RegistryObject<SoundEvent> EXPLOSION_HIT = REGISTRY.register("explode_hit", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("shatteredsoul", "explode_hit")));
 	public static final RegistryObject<SoundEvent> RICOCHET = REGISTRY.register("ricochet", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("shatteredsoul", "ricochet")));
 }
